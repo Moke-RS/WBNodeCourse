15c1af0dd4e36f1aaddf96de8bcb56bc824230ee
Moke <lesigo@gmail.com>
2017-06-19 22:58:35 -0300

    Stopped at Core Concept - Templating

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  // const jsonData = { name: 'Moke', age: 34 };
+  // res.json(jsonData);
+  // res.send(req.query.name);
+  // res.send(req.query);
+  res.render('hello', {
+    name: 'Moke',
+    age: 34
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
